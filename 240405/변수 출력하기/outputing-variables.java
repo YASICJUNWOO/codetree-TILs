39fb964af597f2ca2e2701f9c35e0355379d58ba
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-05 20:21:04 +0900

    Title: 변수 출력하기, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240405/변수 출력하기/outputing-variables.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int num = 3;
+        char ch = 'C';
+
+        System.out.printf("%d...%s",num,ch);
+    }
+}
