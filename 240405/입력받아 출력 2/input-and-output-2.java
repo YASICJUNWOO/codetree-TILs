5cda49162da108be8c585145f0e6a6015e2ed53a
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-05 23:41:41 +0900

    Title: 입력받아 출력 2, Time: 233ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240405/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.useDelimiter("-");
+
+        String num1 = sc.next(), num2 = sc.next();
+
+        System.out.printf("%s%s",num1,num2);
+    }
+}
