2f46ef39d57f46578dedb04e577b067315d5452c
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-06 15:46:57 +0900

    Title: 살 수 있는 물건 2, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240406/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int money = sc.nextInt();
+
+        String item;
+        if(money >= 3000){
+            item = "book";
+        }
+        else if(money >= 1000){
+            item = "mask";
+        }
+        else if(money >= 500){
+            item = "pen";
+        }
+        else{
+            item = "no";
+        }
+
+        System.out.print(item);
+    }
+}
