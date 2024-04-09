242048b2cd11b724c95965aa5fd2965fb1deb6c6
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-09 20:15:05 +0900

    Title: 369 게임, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240409/369 게임/369-game.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1;i<= n;i++){
+
+            int result = -1;
+            if(i%3==0){
+                result = 0;
+            }
+            else{
+                int num = i;
+                int check;
+
+                while(num>0){
+                    check = i/10;
+                    if(check ==3 || check == 6 || check ==9){
+                        result = 0;
+                        break;
+                    }
+                    num /= 10;
+                }
+
+            }
+
+            System.out.print(result == 0 ? "0 " : i+" ");
+        }
+    }
+}
