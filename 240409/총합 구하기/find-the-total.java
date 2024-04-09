1e29ca835535dca1229efb8785259442cee0d738
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-09 22:13:36 +0900

    Title: 총합 구하기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/총합 구하기/find-the-total.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a =sc.nextInt(), b = sc.nextInt();
+
+        int sumValue = 0;
+
+        for(;a<=b;a++){
+            if(a%6==0 && a%8!=0){
+                sumValue += a;
+            }
+        }
+
+        System.out.print(sumValue);
+    }
+}
