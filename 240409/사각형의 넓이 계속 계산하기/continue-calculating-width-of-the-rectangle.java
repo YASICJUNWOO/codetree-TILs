ee8bcff83c645e717de905d0f899f994dee1b30f
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-09 23:21:02 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc  = new Scanner(System.in);
+
+        while(true){
+            int garo = sc.nextInt(), sero = sc.nextInt();
+            String str = sc.next();
+
+            System.out.println(garo*sero);
+
+            if(str.equals("C")){
+                break;
+            }
+
+
+        }
+    }
+}
