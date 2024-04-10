c92579fcc054c4cd40d0041f71fd9fd87d48939c
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-10 11:58:32 +0900

    Title: 10개 입력받기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240410/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc =new Scanner(System.in);
+
+        int idx = 10, sumVal = 0, cnt =0;
+
+        while(idx-->0){
+            int input = sc.nextInt();
+
+            if(input ==0){
+                break;
+            }
+
+            sumVal+=input;
+            cnt++;
+
+        }
+
+        System.out.printf("%d %.1f", sumVal, (double)sumVal/cnt);
+    }
+}
