bfcca1e9563f62bae9c23fe444373056cb289596
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-10 18:16:42 +0900

    Title: 첫 번째와 두 번째 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240410/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int len = str.length();
+
+        char first = str.charAt(0), second = str.charAt(1);
+
+        char[] arr = str.toCharArray();
+
+        for(int i =0;i<len;i++){
+            if(arr[i] == first){
+                arr[i] = second;
+            }
+            else if(arr[i]==second){
+                arrp[i] =first;
+            }
+        }
+
+        System.out.print(String.valueOf(arr));
+    }
+}
