4af15586c097f29cfc3b109dd5fc36d37286cd5f
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-11 02:25:31 +0900

    Title: e 제거하기, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240411/e 제거하기/e-to-remove.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int idx = str.indexOf("e");
+
+        String result = str.substring(0,idx)+str.substring(idx+1);
+
+        System.out.print(result);
+    }
+}
