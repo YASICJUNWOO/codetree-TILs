cd39798d69efaead44de5fa9d8efdd2ca8bcc3b6
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-12 19:49:17 +0900

    Title: 별표 출력하기 7, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240412/별표 출력하기 7/print-star-7.java
+++ 240412/별표 출력하기 7/print-star-7.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
 
-        Scanner sc = new Scanner(Systrm.in);
+        Scanner sc = new Scanner(System.in);
 
-        int n = sc.next();
+        int n = sc.nextInt();
 
         for(int i =0;i<n;i++){
             for(int j =0;j<i+1;j++){
