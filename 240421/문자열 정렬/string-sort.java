4543a88379f5a6b88426de7f8ce59fcfe31657a1
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-21 01:34:46 +0900

    Title: 문자열 정렬, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240421/문자열 정렬/string-sort.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc =  new Scanner(System.in);
+
+        String str = sc.next();
+
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        String newStr = new String(arr);
+
+        System.out.print(arr);
+    }
+}
