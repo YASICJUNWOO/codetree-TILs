1e304d16667066598e4a2f9bc27a16f5a51bfe62
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-22 12:18:32 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240422/G or H 3/G-or-H-3.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+
+    static int size = 0;
+    static int range = 0;
+    static int[] arr;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc =new Scanner(System.in);
+
+        size = sc.nextInt();
+        range = sc.nextInt();
+
+        arr = new int[10000];
+
+        for(int i = 0;i<size;i++){
+            int idx = sc.nextInt();
+            char  ch = sc.next().charAt(0);
+            if(ch == 'G'){
+                arr[idx-1] = 1;
+            }
+            else{
+                arr[idx-1] =2;
+            }
+        }
+
+        int maxVal = Integer.MIN_VALUE;
+        for(int i = 0;i<10000-range+1;i++){
+            //System.out.print( arr[i]);
+
+            maxVal = Math.max(maxVal, sum(i));
+        }
+
+        System.out.print(maxVal);
+
+    }
+
+    public static int sum(int idx){
+        int sum =0;
+        for(int  i =idx ; i<=idx+range;i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
