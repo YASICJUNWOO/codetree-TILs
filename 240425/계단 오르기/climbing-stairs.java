e85d59b53ad1b273cca2432d8b6d7dc8b81ce9d8
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-25 00:34:19 +0900

    Title: 계단 오르기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240425/계단 오르기/climbing-stairs.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int target = sc.nextInt();
+
+        int[] arr = new int[target+1];
+        arr[0] = 1;
+        arr[1] = 0;
+        arr[2] = 1;
+
+        for(int i = 3 ; i<=target ;i++){
+            arr[i] = arr[i-2]+arr[i-3];
+        }
+
+        System.out.print(arr[target]);
+    }
+}
