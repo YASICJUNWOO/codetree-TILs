a72e7842f01ec8c211c9a7ebd026e3ed6e4d061b
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2024-04-24 13:16:10 +0900

    Title: 최소 경로로 탈출 하기, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240424/최소 경로로 탈출 하기/escape-with-min-distance.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+class Info{
+    int row;
+    int col;
+    int depth;
+
+    public Info(int row, int col, int depth){
+        this.row = row;
+        this.col = col;
+        this.depth = depth;
+    }
+}
+public class Main {
+
+    static int rowSize = 0, colSize = 0;
+    static int[][] map;
+    static boolean[][] visit;
+    static Queue<Info> q = new LinkedList<>();
+
+    static int[] dRow = {-1,1,0,0};
+    static int[] dCol = {0,0,-1,1};
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc  = new Scanner(System.in);
+
+        rowSize = sc.nextInt();
+        colSize = sc.nextInt();
+
+        map = new int[rowSize][colSize];
+        visit = new boolean[rowSize][colSize];
+
+        for(int i = 0  ;i<rowSize;i++){
+            for(int j = 0;j<colSize;j++){
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        q.add(new Info(0,0,0));
+        visit[0][0] = true;
+        bfs();
+
+    }
+
+    public static void bfs(){
+
+        while(!q.isEmpty()){
+
+            Info info = q.poll();
+            int row = info.row;
+            int col = info.col;
+            int depth = info.depth;
+
+            if(row == rowSize-1 && col == colSize-1){
+                System.out.print(depth);
+                return;
+            }
+
+            for(int i = 0 ;i<4;i++){
+                int targetRow = row + dRow[i];
+                int targetCol = col + dCol[i];
+                
+
+                if(canGo(targetRow,targetCol) && !visit[targetRow][targetCol]){
+                    visit[targetRow][targetCol] = true;
+                    q.add(new Info(targetRow,targetCol,depth+1));
+                }
+            }
+        }
+
+    }
+
+    public static boolean canGo(int row, int col){
+        return row>=0 && col>=0 && row<rowSize && col<colSize && map[row][col] != 0;
+    }
+}
